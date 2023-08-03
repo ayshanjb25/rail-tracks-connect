6e282094e3d1ce16426214220c3bbc1286f72c26
ayshanjb25 <ayshanjb25@gmail.com>
2023-08-03 19:35:40 +0530

    Database Migrations

---
+++ public/js/app.js
@@ -0,0 +1,169 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.jsx":
+/*!******************************!*\
+  !*** ./resources/js/app.jsx ***!
+  \******************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\resources\\js\\app.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (9:3):\n\n\u001b[0m \u001b[90m  7 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m  8 |\u001b[39m \u001b[33mReactDOM\u001b[39m\u001b[33m.\u001b[39mcreateRoot(document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'app'\u001b[39m))\u001b[33m.\u001b[39mrender(\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  9 |\u001b[39m   \u001b[33m<\u001b[39m\u001b[33mApp\u001b[39m\u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m   \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 10 |\u001b[39m )\u001b[0m\n\u001b[0m \u001b[90m 11 |\u001b[39m      \u001b[0m\n\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\n    at instantiate (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:63:32)\n    at constructor (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:358:12)\n    at Parser.raise (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:3255:19)\n    at Parser.expectOnePlugin (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:3301:18)\n    at Parser.parseExprAtom (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:11245:18)\n    at Parser.parseExprSubscripts (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10890:23)\n    at Parser.parseUpdate (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10873:21)\n    at Parser.parseMaybeUnary (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10849:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10687:61)\n    at Parser.parseExprOps (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10692:23)\n    at Parser.parseMaybeConditional (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10669:23)\n    at Parser.parseMaybeAssign (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10630:21)\n    at C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10600:39\n    at Parser.allowInAnd (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12280:12)\n    at Parser.parseMaybeAssignAllowIn (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10600:17)\n    at Parser.parseExprListItem (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12032:18)\n    at Parser.parseCallExpressionArguments (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:11087:22)\n    at Parser.parseCoverCallAndAsyncArrowHead (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10997:29)\n    at Parser.parseSubscript (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10932:19)\n    at Parser.parseSubscripts (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10903:19)\n    at Parser.parseExprSubscripts (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10894:17)\n    at Parser.parseUpdate (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10873:21)\n    at Parser.parseMaybeUnary (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10849:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10687:61)\n    at Parser.parseExprOps (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10692:23)\n    at Parser.parseMaybeConditional (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10669:23)\n    at Parser.parseMaybeAssign (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10630:21)\n    at Parser.parseExpressionBase (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10584:23)\n    at C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10580:39\n    at Parser.allowInAnd (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12275:16)\n    at Parser.parseExpression (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:10580:17)\n    at Parser.parseStatementContent (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12733:23)\n    at Parser.parseStatementLike (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12584:17)\n    at Parser.parseModuleItem (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12561:17)\n    at Parser.parseBlockOrModuleBlockBody (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:13185:36)\n    at Parser.parseBlockBody (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:13178:10)\n    at Parser.parseProgram (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12460:10)\n    at Parser.parseTopLevel (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:12450:25)\n    at Parser.parse (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:14345:10)\n    at parse (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\parser\\lib\\index.js:14386:38)\n    at parser (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\core\\lib\\parser\\index.js:41:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\core\\lib\\transformation\\normalize-file.js:64:38)\n    at normalizeFile.next (<anonymous>)\n    at run (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\core\\lib\\transformation\\index.js:21:50)\n    at run.next (<anonymous>)\n    at transform (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\@babel\\core\\lib\\transform.js:22:41)\n    at transform.next (<anonymous>)\n    at step (C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\gensync\\index.js:261:32)\n    at C:\\xampp\\htdocs\\RailTracksConnect_App\\rail-tracks-connect\\node_modules\\gensync\\index.js:273:13");
+
+/***/ }),
+
+/***/ "./resources/css/app.css":
+/*!*******************************!*\
+  !*** ./resources/css/app.css ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/app": 0,
+/******/ 			"css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.jsx")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
