8dbf5294a75284ebf8677890af5fce787b0266c0
Dilushini <diluhewa@gmail.com>
2023-08-07 22:44:52 +0530

    Login API

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+        }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $validator = Validator::make($input,[
+            'name' => 'required',
+            'email'=> 'required',
+            'password'=> 'required'
+        ]);
+        if($validator->fails()){
+            return $this->sendError('Validation Error', $validator->errors());
+        }
+        $user = User::create($input);
+        return response()->json([
+            'success' => true,
+            'message' => 'User Record Created Successfully',
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if(User::where('id',$id)->exists()){
+            $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = $request->password;
+            $user->save();
+            return response()->json([
+                'message' => 'User Record Updated Successfully'
+            ],200);
+        }
+        else{
+            return response()->json([
+                'message' => 'User Record Not Found'
+            ],404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(User::where('id',$id)->exists()){
+            $user = User::find($id);
+            $user = delete();
+            return response()->json([
+                'message' => 'User Record Deleted Successfully'
+            ],200);
+        }
+        else{
+            return response()->json([
+                'message' => 'User Record Not Found'
+            ],404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
-
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Controllers\PassengerController;
+// use Illuminate\Http\Controllers\PassengerController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,18 +17,29 @@ use Illuminate\Http\Controllers\PassengerController;
 |
 */
 
+
+
+Route::get('/users',function(){
+    return UserResource::collection(User::all());
+});
+Route::get('/user/{id}',function($id){
+    return new UserResource(User::findOrFail($id));
+});
+Route::post('/user', [UserController::class,'store']);
+
+Route::put('/user/{id}', [UserController::class,'update']);
+Route::delete('/user/{id}', [UserController::class,'destroy']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::resource('passengers', PassengerController::class);
+// Route::resource('passengers', PassengerController::class);
 
 
 
-Route::post('login', [UserController::class, 'login']);
-Route::post('register', [PassengerController::class, 'register']);
+// Route::post('login', [UserController::class, 'login']);
+// Route::post('register', [PassengerController::class, 'register']);
 
-Route::put('passengers/{id}', [PassengerController::class, 'update']);
-Route::delete('passengers/{id}', [PassengerController::class, 'delete']);
+// Route::put('passengers/{id}', [PassengerController::class, 'update']);
+// Route::delete('passengers/{id}', [PassengerController::class, 'delete']);
 
